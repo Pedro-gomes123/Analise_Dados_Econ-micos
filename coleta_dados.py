70a216084af986b693d5d5dc24daaceb31e16190
Pedro <pedrocarvalhog10@gmail.com>
2025-08-21 23:17:57 -0300

    subindo para poder rodar

---
+++ coleta_dados.py
@@ -0,0 +1,43 @@
+import requests
+import pandas as pd 
+import datetime 
+import requests as rq
+import json as j
+
+codigo_serie = ['selic','dolar','ipca' ]
+
+nome_serie = [432, 1, 433]
+
+indicadores = {
+    'selic': 432,
+    'dolar': 1,
+    'ipca': 433
+    }
+
+
+def Buscar_dados(codigo_serie, nome_serie):
+    data_atual = datetime.datetime.now() # pega no sistema a data do dia 
+    data_duracao =datetime.timedelta(day=365) # pega no sistema o calendario do ano 
+    data_inicio = data_atual - data_duracao
+
+
+    data_atual_formatada =  data_atual.strftime('%d/%m/%Y')
+    data_inicio_formatada = data_inicio.strftime('%d/%m/%Y')
+
+    url = f'https://api.bcb.gov.br/dados/serie/bcdata.sgs.{codigo_serie}/dados?formato=json&dataInicial={data_inicio_formatada}&dataFinal={data_atual_formatada }'
+
+    response = rq.get(url)
+    
+
+    if response.status_code == 200:
+
+        print('response funcionando')
+        Dados_variaveis = response.json()
+        df = pd.DataFrame(Dados_variaveis)
+        print(df.head())
+        
+    else:
+
+        print(f"response erro {response}")
+    
+Buscar_dados()
